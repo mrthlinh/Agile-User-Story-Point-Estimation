56457c44a47b22d31dda26501d7a6cf229de24e5
Linh Hoang Truong <mrthlinh@gmail.com>
2018-12-03 16:36:14 -0600

    minor updates

--- Word2VecFeature.py
+++ Word2VecFeature.py
@@ -10,37 +10,53 @@ from gensim.test.utils import common_texts,get_tmpfile
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from gensim.models import Word2Vec
 import pandas as pd
+from multiprocessing import Pool
+
+#path = get_tmpfile("word2vec.model")
+def Word2VecFeature(embedding_size = 200):
 
 
-path = get_tmpfile("word2vec.model")
-corpus = pd.read_hdf('helper/corpus_hdf', key='abc', mode='r')
-
-model = Word2Vec(corpus, size=400, window=3, min_count=1, workers=4)
-model.train(corpus,len(corpus),epochs = 10)
-
-model.save("helper/word2vec.model")
-
-#    model = gensim.models.Word2Vec(
-#        documents,
-#        size=150,
-#        window=10,
-#        min_count=2,
-#        workers=10)
-#    model.train(documents, total_examples=len(documents), epochs=10)
-
-features = []
-
-for i in range(len(corpus)):    
-    sentence = corpus.iloc[i]
-    ave_vector = 0
-    for s in sentence:
-#        print(s)
-        ave_vector += model.wv[s]
+    corpus = pd.read_hdf('helper/corpus_hdf', key='abc', mode='r')
     
-    ave_vector = ave_vector / model.vector_size
+    #corpus = corpus['concat']
+#    window_size = 3
+    model = Word2Vec(corpus['concat'], size=embedding_size, window=3, min_count=1, workers=4)
+    #model.train(corpus,len(corpus),epochs = 10)
     
-    features.append(ave_vector)
-columns = ['feature'+str(i) for i in range(model.vector_size)]
-df_word2vec = pd.DataFrame.from_records(features,columns=columns)
-df_word2vec.to_csv("features/word2vec_ave.csv")
+    model.save("helper/word2vec_"+str(embedding_size)+".model")
+    
+    features = []
+    
+    for i in range(len(corpus)):    
+        sentence = corpus.loc[i,'concat']
+        ave_vector = 0
+        for s in sentence:
+    #        print(s)
+            ave_vector += model.wv[s]
+        
+        ave_vector = ave_vector / model.vector_size
+        
+        features.append(ave_vector)
+        
+    columns = ['feature'+str(i) for i in range(model.vector_size)]
+    df_word2vec = pd.DataFrame.from_records(features,columns=columns)
+    df_word2vec = df_word2vec.join(corpus['project'])
+    
+    print("Check Null")
+    assert(df_word2vec.isnull().any().any() == False)
+    
+    df_word2vec.to_csv("features/word2vec_ave_"+str(embedding_size)+".csv")
 
+
+def main():
+    embedding_size = [10,50,100,300,500,1000,2000]
+    proc = 16
+    print("Extracture Features")
+    with Pool(proc) as p:
+    #    length = len(embedding_size)
+        feature = p.map(Word2VecFeature, embedding_size)
+
+    
+if __name__ == "__main__":
+    main()
+    
